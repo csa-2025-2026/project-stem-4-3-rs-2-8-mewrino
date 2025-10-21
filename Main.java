5c2236faaa866083bd7f355003f81140e2c15b8a
mewrino <michellenguyen04272008@gmail.com>
2025-10-21 21:46:47 +0000

    d

--- Main.java
+++ Main.java
@@ -4,6 +4,25 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    // Problem 2
+
+    int counter = 0;
+    
+
+    for (int i = 17; i <= 73; i++)
+    {
+      if (counter % 10 == 0)
+      {
+        System.out.println(i + " ");
+
+    }
+    else
+    {
+      System.out.print(i + " ");
+    }
+    counter++;
+
   }
+
+}
 }
